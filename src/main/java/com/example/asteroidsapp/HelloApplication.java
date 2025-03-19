b5e50e78dc16f4efa477f28e6277028fe8fd4840
RedGalm <65945889+RedGalm@users.noreply.github.com>
2025-03-20 00:30:43 +0100

    Changes

--- src/main/java/com/example/asteroidsapp/HelloApplication.java
+++ src/main/java/com/example/asteroidsapp/HelloApplication.java
@@ -13,8 +13,11 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
 
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
         stage.setTitle("Hello!");
+
         stage.setScene(scene);
+
         stage.show();
     }
 
