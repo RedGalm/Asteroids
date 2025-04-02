f01d365e976909405a394ff6c30f0a8ecc88f4d9
RedGalm <65945889+RedGalm@users.noreply.github.com>
2025-04-02 13:44:49 +0200

    Functionality added

--- src/main/java/com/example/asteroidsapp/AsteroidsApp.java
+++ src/main/java/com/example/asteroidsapp/AsteroidsApp.java
@@ -2,9 +2,14 @@ package com.example.asteroidsapp;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.input.KeyCode;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -13,33 +18,57 @@ import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
+import static javafx.application.Platform.exit;
+
 public class AsteroidsApp extends Application {
     public static int width = 450;
     public static int height = 300;
 
     public void start(Stage stage) throws IOException {
+        AtomicInteger points = new AtomicInteger();
+        BorderPane startScreenLayout = new BorderPane();
+        Text welcomeMessage1 = new Text("Welcome to the Asteroids game!");
+        Text welcomeMessage2 = new Text("Press Start to begin");
+        Button startButton = new Button("Start");
+        VBox vBox1 = new VBox();
+        vBox1.getChildren().addAll(welcomeMessage1, welcomeMessage2, startButton);
+        vBox1.setAlignment(Pos.CENTER);
+        vBox1.setSpacing(20.0);
+        startScreenLayout.setCenter(vBox1);
+        Scene startScreen = new Scene(startScreenLayout, width, height);
+
+        BorderPane endScreenLayout = new BorderPane();
+        Text endMessage = new Text("Game Over! Your score: 0");
+        Button exit = new Button("Exit game");
+        VBox vBox2 = new VBox();
+        vBox2.getChildren().addAll(endMessage, exit);
+        vBox2.setAlignment(Pos.CENTER);
+        vBox2.setSpacing(20.0);
+        endScreenLayout.setCenter(vBox2);
+        Scene endScreen = new Scene(endScreenLayout, width, height);
+
         Pane pane = new Pane();
         Ship ship = new Ship(width / 2, height / 2);
         Text text = new Text(10, 20, "Points: 0");
-        AtomicInteger points = new AtomicInteger();
         pane.getChildren().add(text);
         pane.getChildren().add(ship.getCharacter());
-        Scene scene = new Scene(pane, width, height);
+        Scene game = new Scene(pane, width, height);
         Random random = new Random();
 
         Map<KeyCode, Boolean> keys = new HashMap<>();
         List<Asteroid> asteroids = new ArrayList<>();
         List<Projectile> projectiles = new ArrayList<>();
         List<Asteroid> disappearedAsteroids = new ArrayList<>();
+
         for (int i = 0; i < 8; i++) {
             asteroids.add(new Asteroid(random.nextInt(width / 3), random.nextInt(height)));
         }
         asteroids.forEach(asteroid -> pane.getChildren().add(asteroid.getCharacter()));
 
-        scene.setOnKeyPressed(event -> {
+        game.setOnKeyPressed(event -> {
             keys.put(event.getCode(), Boolean.TRUE);
         });
-        scene.setOnKeyReleased(event -> {
+        game.setOnKeyReleased(event -> {
             keys.put(event.getCode(), Boolean.FALSE);
         });
 
@@ -74,6 +103,7 @@ public class AsteroidsApp extends Application {
                     asteroid.move();
                     if (asteroid.collide(ship)) {
                         stop();
+                        stage.setScene(endScreen);
                     }
                 });
                 ship.move();
@@ -91,6 +121,7 @@ public class AsteroidsApp extends Application {
                         asteroids.remove(collided);
                         removableProjectiles.add(projectile);
                         text.setText("Points: " + points.addAndGet(100));
+                        endMessage.setText("Game Over! Your score: " + points.addAndGet(100));
 
                     });
 
@@ -117,7 +148,15 @@ public class AsteroidsApp extends Application {
 
 
         stage.setTitle("Asteroids!");
-        stage.setScene(scene);
+        stage.setScene(startScreen);
+        startButton.setOnAction(event -> {
+            stage.setScene(game);
+        });
+
+        exit.setOnAction(event -> {
+            exit();
+        });
+
         stage.show();
     }
 
